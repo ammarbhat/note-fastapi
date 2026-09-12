1b7ba355106ac1e6cf221d8d6ff690a422e290f5
ammarbhat <bhatmmr@gmail.com>
2026-09-12 15:01:13 +0530

    Add user table to db

--- src/crud/models.py
+++ src/crud/models.py
@@ -1,9 +1,19 @@
 from datetime import date
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from crud.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    email: Mapped[str]
+    hash_password: Mapped[str]
+
+    notes: Mapped[list["Note"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
 
 
 class Note(Base):
@@ -13,3 +23,5 @@ class Note(Base):
     task: Mapped[str]
     status: Mapped[bool]
     event_date: Mapped[date]
+
+    users: Mapped["User"] = relationship(back_populates="notes")
