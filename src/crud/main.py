5e4e07c01b32413b8dd71c0a6789a0828d508068
ammarbhat <bhatmmr@gmail.com>
2026-09-13 16:36:18 +0530

    More functions needed for token endpoint

--- src/crud/main.py
+++ src/crud/main.py
@@ -1,34 +1,40 @@
 from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing import Annotated
-from datetime import datetime, date
+from datetime import datetime, date, timedelta, timezone
 from crud.models import Note, User
-from crud.schemas import NoteBase, EditBase, UserBase, Classified
+from crud.schemas import NoteBase, EditBase, UserBase, Classified, TokenData, Token
 from crud.database import engine, Base, get_db
 from pwdlib import PasswordHash
 from jwt.exceptions import InvalidTokenError
 import jwt
+
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 SECRET_KEY = "d158cfbc72dc60929a36c29d9d3eea4d405403328f4f4e9e289ddfba1d35e54f"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 pwdhasher = PasswordHash.recommended()
 DUMMY_HASH = pwdhasher.hash("dummypassword")
 
+
 def verify_password(password, hashed_password):
     return pwdhasher.verify(hashed_password, password)
+
 
 def get_hash(password):
     return pwdhasher.hash(password)
 
+
 def get_user(db, username: str):
     user = db.query(User).filter(User.username == username).first()
     if user is None:
-       return False
+        return False
     return user
+
 
 def authenticate_user(db, username: str, password: str):
     user = get_user(db, username)
@@ -36,6 +42,37 @@ def authenticate_user(db, username: str, password: str):
         verify_password(password, DUMMY_HASH)
     if not verify_password(password, user.hash_password):
         return False
+    return user
+
+
+def create_acess_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: Annotated[str, Depends(oauth_scheme)], db=Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -89,16 +126,18 @@ def update_note(note_id: int, edits: EditBase, db=Depends(get_db)):
 
 
 @app.post("/users/")
-def add_user(user: UserBase,body: Classified, db=Depends(get_db)):
+def add_user(user: UserBase, body: Classified, db=Depends(get_db)):
     new_user = User(
         username=user.username,
         email=user.email,
         hash_password=pwdhasher.hash(body.password),
     )
     test_user = db.query(User).filter(User.username == user.username).first()
-    if  test_user:
-     if user.username == test_user.username:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="user already exists")
+    if test_user:
+        if user.username == test_user.username:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="user already exists"
+            )
     db.add(new_user)
     db.commit()
 
@@ -109,8 +148,11 @@ def delete_user(username: str, body: Classified, db=Depends(get_db)):
     if user is None:
         raise HTTPException(status_code=404, detail="user not found")
     elif not pwdhasher.verify(body.password, user.hash_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid username or password")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="invalid username or password",
+        )
     else:
         db.delete(user)
         db.commit()
-        return {"message" : "User deleted"}
+        return {"message": "User deleted"}
